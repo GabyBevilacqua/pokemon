960300c68089ab528bfd0da316a12f8cf3747112
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-07-24 20:16:12 +0200

    agregado canvas camara y demas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+"use client";
+import { useEffect } from "react"
+import { Scene, WebGLRenderer, PerspectiveCamera, Mesh, MeshBasicMaterial, BoxGeometry } from "three";
+
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+
+    useEffect(() => {
+        const scene = new Scene()
+        const renderer = new WebGLRenderer({
+            antialias: true,
+            canvas: document.getElementById("bg")
+        });
+        const camera = new PerspectiveCamera(
+            50, 
+            window.innerWidth / window.innerHeight, 
+            0.1, 
+            1000
+        );
+
+        // mover camara
+        camera.position.set(0, 0, 5);
+
+        // creando el cubo
+        const geometry = new BoxGeometry(1, 1, 1);
+        const material = new MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new Mesh(geometry, material);
+        scene.add(cube);
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        function animate() {
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+        }
+        animate();
+    }, []);
+
+  return <canvas id="bg" />;
 }
